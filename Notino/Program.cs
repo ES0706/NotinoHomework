2e886a6d9cbab838a19031979b9823e50ae3e5e5
Erik Schmidt <erik.schmidt@quest.com>
2021-12-14 11:29:29 +0100

    comments

--- Notino/Program.cs
+++ Notino/Program.cs
@@ -3,25 +3,6 @@ using Microsoft.Extensions.Hosting;
 
 namespace Notino
 {
-    //class Program
-    //{
-    //    static async Task Main(string[] args)
-    //    {
-    //        var sourcePath = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\SourceFiles\\Document1.xml");
-    //        var targetPath = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\TargetFiles\\Document1.json");
-
-    //        var converterProvider = new XmlToJsonConverterProvider();
-    //        var writerProvider = new FileWriterProvider();
-    //        var readerProvider = new FileReaderProvider();
-    //        var converterService = new ConverterService(converterProvider, writerProvider, readerProvider);
-
-    //        var convertResult = await converterService.ConvertSourceToTarget(sourcePath, targetPath);
-    //        if (convertResult.Failed)
-    //            Console.WriteLine(convertResult.Errors);
-    //    }
-    //}
-
-
     public class Program
     {
         public static void Main(string[] args)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Notino Zadanie
+## Poznánky ku kódu v zadaní
+Je šitý len na konkrétne dva súbory, chýba abstrakcia. Chýba validácia xml a hodnôt, ktoré má obsahovať. Napriek exception z predošlého čítania xml sa pokračuje v Parsovaní xml, čo určite povedie k unhandled Exception ak predošlý kód zlyhá.
